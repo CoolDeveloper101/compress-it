5e11b8030b7d584818059c0f4e2c3947447a2af5
Ashwin Gupta <cooldeveloper101@gmail.com>
2021-05-14 17:04:16 +0530

    Adding check if wasm-pack already exists.

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 rustup toolchain install stable
-ls "$HOME/.cargo/bin"
+FILE="$HOME/.cargo/bin/wasm-pack"
+if [ -f "$FILE" ]; then
+    echo "The wasm pack binary already exists. No nead to reinstall it."
+else
+	cargo install wasm-pack
+
 wasm-pack build --target web
