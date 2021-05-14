f4b04334869afbfcbb091527ac8e78f9a2d5def7
Ashwin Gupta <cooldeveloper101@gmail.com>
2021-05-14 12:08:10 +0530

    Adding pkg

---
+++ pkg/compress_it.d.ts
@@ -0,0 +1,53 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {Uint8Array} input
+* @returns {Uint8Array}
+*/
+export function snappy_compress(input: Uint8Array): Uint8Array;
+/**
+* @param {Uint8Array} input
+* @returns {Uint8Array}
+*/
+export function snappy_decompress(input: Uint8Array): Uint8Array;
+/**
+* @param {number} input_len
+* @returns {number}
+*/
+export function snappy_max_compress_len(input_len: number): number;
+/**
+* @param {Uint8Array} input
+* @returns {number}
+*/
+export function snappy_decompress_len(input: Uint8Array): number;
+/**
+* @param {string} filename
+* @param {Uint8Array} input
+* @returns {Uint8Array}
+*/
+export function gzip_compress(filename: string, input: Uint8Array): Uint8Array;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly snappy_compress: (a: number, b: number, c: number) => void;
+  readonly snappy_decompress: (a: number, b: number, c: number) => void;
+  readonly snappy_max_compress_len: (a: number) => number;
+  readonly snappy_decompress_len: (a: number, b: number) => number;
+  readonly gzip_compress: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_free: (a: number, b: number) => void;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+}
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ pkg/compress_it_bg.wasm.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function snappy_compress(a: number, b: number, c: number): void;
+export function snappy_decompress(a: number, b: number, c: number): void;
+export function snappy_max_compress_len(a: number): number;
+export function snappy_decompress_len(a: number, b: number): number;
+export function gzip_compress(a: number, b: number, c: number, d: number, e: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_free(a: number, b: number): void;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
